b0d28452e644db3ef844c8f42ea3931841cfc5f5
yhamo <hamouda.yassin@hotmai.fr>
2014-11-18 10:53:32 +0100

    1

--- Presentation/Fsaisie.Designer.cs
+++ Presentation/Fsaisie.Designer.cs
@@ -43,16 +43,23 @@
             this.tb_ca = new System.Windows.Forms.TextBox();
             this.btn_annuler = new System.Windows.Forms.Button();
             this.btn_valider = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.cb_MemeValeur = new System.Windows.Forms.CheckBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.gb_infosProjet = new System.Windows.Forms.GroupBox();
+            this.gb_infosAnnée = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.tb_ca_annee = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.gb_infosProjet.SuspendLayout();
+            this.gb_infosAnnée.SuspendLayout();
             this.SuspendLayout();
             // 
             // tb_inv
             // 
-            this.tb_inv.Location = new System.Drawing.Point(272, 96);
+            this.tb_inv.Location = new System.Drawing.Point(272, 31);
             this.tb_inv.Name = "tb_inv";
             this.tb_inv.Size = new System.Drawing.Size(100, 20);
             this.tb_inv.TabIndex = 0;
@@ -60,7 +67,7 @@
             // 
             // tb_ann
             // 
-            this.tb_ann.Location = new System.Drawing.Point(623, 389);
+            this.tb_ann.Location = new System.Drawing.Point(272, 155);
             this.tb_ann.Name = "tb_ann";
             this.tb_ann.Size = new System.Drawing.Size(100, 20);
             this.tb_ann.TabIndex = 1;
@@ -68,7 +75,7 @@
             // 
             // tb_txAct
             // 
-            this.tb_txAct.Location = new System.Drawing.Point(272, 200);
+            this.tb_txAct.Location = new System.Drawing.Point(272, 115);
             this.tb_txAct.Name = "tb_txAct";
             this.tb_txAct.Size = new System.Drawing.Size(100, 20);
             this.tb_txAct.TabIndex = 2;
@@ -76,7 +83,7 @@
             // 
             // tb_cf
             // 
-            this.tb_cf.Location = new System.Drawing.Point(623, 193);
+            this.tb_cf.Location = new System.Drawing.Point(174, 68);
             this.tb_cf.Name = "tb_cf";
             this.tb_cf.Size = new System.Drawing.Size(100, 20);
             this.tb_cf.TabIndex = 3;
@@ -84,7 +91,7 @@
             // 
             // tb_cv
             // 
-            this.tb_cv.Location = new System.Drawing.Point(623, 225);
+            this.tb_cv.Location = new System.Drawing.Point(174, 100);
             this.tb_cv.Name = "tb_cv";
             this.tb_cv.Size = new System.Drawing.Size(100, 20);
             this.tb_cv.TabIndex = 4;
@@ -93,8 +100,8 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(95, 95);
+            this.label1.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic);
+            this.label1.Location = new System.Drawing.Point(94, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(158, 19);
             this.label1.TabIndex = 5;
@@ -103,8 +110,8 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(446, 381);
+            this.label2.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic);
+            this.label2.Location = new System.Drawing.Point(132, 156);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(119, 19);
             this.label2.TabIndex = 6;
@@ -113,8 +120,8 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(95, 195);
+            this.label3.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic);
+            this.label3.Location = new System.Drawing.Point(118, 117);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(134, 19);
             this.label3.TabIndex = 7;
@@ -124,7 +131,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(446, 188);
+            this.label4.Location = new System.Drawing.Point(44, 66);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(102, 19);
             this.label4.TabIndex = 8;
@@ -134,7 +141,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(446, 224);
+            this.label5.Location = new System.Drawing.Point(19, 101);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(127, 19);
             this.label5.TabIndex = 9;
@@ -154,8 +161,8 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(95, 371);
+            this.label7.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic);
+            this.label7.Location = new System.Drawing.Point(142, 238);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(109, 19);
             this.label7.TabIndex = 11;
@@ -163,7 +170,7 @@
             // 
             // tb_ca
             // 
-            this.tb_ca.Location = new System.Drawing.Point(272, 379);
+            this.tb_ca.Location = new System.Drawing.Point(272, 237);
             this.tb_ca.Name = "tb_ca";
             this.tb_ca.Size = new System.Drawing.Size(100, 20);
             this.tb_ca.TabIndex = 12;
@@ -171,7 +178,7 @@
             // 
             // btn_annuler
             // 
-            this.btn_annuler.Location = new System.Drawing.Point(134, 475);
+            this.btn_annuler.Location = new System.Drawing.Point(309, 478);
             this.btn_annuler.Name = "btn_annuler";
             this.btn_annuler.Size = new System.Drawing.Size(75, 23);
             this.btn_annuler.TabIndex = 13;
@@ -181,7 +188,7 @@
             // 
             // btn_valider
             // 
-            this.btn_valider.Location = new System.Drawing.Point(284, 475);
+            this.btn_valider.Location = new System.Drawing.Point(613, 478);
             this.btn_valider.Name = "btn_valider";
             this.btn_valider.Size = new System.Drawing.Size(75, 23);
             this.btn_valider.TabIndex = 14;
@@ -189,50 +196,119 @@
             this.btn_valider.UseVisualStyleBackColor = true;
             this.btn_valider.Click += new System.EventHandler(this.btn_valider_Click);
             // 
-            // checkBox1
+            // cb_MemeValeur
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.checkBox1.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.ForeColor = System.Drawing.Color.Red;
-            this.checkBox1.Location = new System.Drawing.Point(70, 419);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(424, 23);
-            this.checkBox1.TabIndex = 15;
-            this.checkBox1.Text = "Cocher cette case si les valeurs sont les mêmes pour chaques année";
-            this.checkBox1.UseVisualStyleBackColor = false;
+            this.cb_MemeValeur.AutoSize = true;
+            this.cb_MemeValeur.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.cb_MemeValeur.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_MemeValeur.ForeColor = System.Drawing.Color.Red;
+            this.cb_MemeValeur.Location = new System.Drawing.Point(188, 434);
+            this.cb_MemeValeur.Name = "cb_MemeValeur";
+            this.cb_MemeValeur.Size = new System.Drawing.Size(424, 23);
+            this.cb_MemeValeur.TabIndex = 15;
+            this.cb_MemeValeur.Text = "Cocher cette case si les valeurs sont les mêmes pour chaques année";
+            this.cb_MemeValeur.UseVisualStyleBackColor = false;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(125, 304);
+            this.label8.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic);
+            this.label8.Location = new System.Drawing.Point(132, 196);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(84, 13);
+            this.label8.Size = new System.Drawing.Size(119, 19);
             this.label8.TabIndex = 16;
-            this.label8.Text = "Valeur résiduelle";
+            this.label8.Text = "Valeur résiduelle :";
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(89, 149);
+            this.label9.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic);
+            this.label9.Location = new System.Drawing.Point(36, 78);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(162, 13);
+            this.label9.Size = new System.Drawing.Size(216, 19);
             this.label9.TabIndex = 17;
-            this.label9.Text = "Montant investissement matériels";
+            this.label9.Text = "Montant investissement matériels :";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(230, 297);
+            this.textBox1.Location = new System.Drawing.Point(272, 195);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 18;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(272, 142);
+            this.textBox2.Location = new System.Drawing.Point(272, 77);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 19;
+            // 
+            // gb_infosProjet
+            // 
+            this.gb_infosProjet.Controls.Add(this.tb_ann);
+            this.gb_infosProjet.Controls.Add(this.textBox2);
+            this.gb_infosProjet.Controls.Add(this.tb_inv);
+            this.gb_infosProjet.Controls.Add(this.textBox1);
+            this.gb_infosProjet.Controls.Add(this.tb_txAct);
+            this.gb_infosProjet.Controls.Add(this.label9);
+            this.gb_infosProjet.Controls.Add(this.label1);
+            this.gb_infosProjet.Controls.Add(this.label8);
+            this.gb_infosProjet.Controls.Add(this.label2);
+            this.gb_infosProjet.Controls.Add(this.label3);
+            this.gb_infosProjet.Controls.Add(this.label7);
+            this.gb_infosProjet.Controls.Add(this.tb_ca);
+            this.gb_infosProjet.Location = new System.Drawing.Point(12, 97);
+            this.gb_infosProjet.Name = "gb_infosProjet";
+            this.gb_infosProjet.Size = new System.Drawing.Size(437, 316);
+            this.gb_infosProjet.TabIndex = 20;
+            this.gb_infosProjet.TabStop = false;
+            this.gb_infosProjet.Text = "Informations projet";
+            // 
+            // gb_infosAnnée
+            // 
+            this.gb_infosAnnée.Controls.Add(this.checkBox1);
+            this.gb_infosAnnée.Controls.Add(this.label10);
+            this.gb_infosAnnée.Controls.Add(this.tb_ca_annee);
+            this.gb_infosAnnée.Controls.Add(this.tb_cv);
+            this.gb_infosAnnée.Controls.Add(this.tb_cf);
+            this.gb_infosAnnée.Controls.Add(this.label4);
+            this.gb_infosAnnée.Controls.Add(this.label5);
+            this.gb_infosAnnée.Location = new System.Drawing.Point(471, 115);
+            this.gb_infosAnnée.Name = "gb_infosAnnée";
+            this.gb_infosAnnée.Size = new System.Drawing.Size(313, 269);
+            this.gb_infosAnnée.TabIndex = 21;
+            this.gb_infosAnnée.TabStop = false;
+            this.gb_infosAnnée.Text = "informations Pour année n°";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic);
+            this.label10.Location = new System.Drawing.Point(44, 139);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(109, 19);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Chiffre d\'affaire :";
+            // 
+            // tb_ca_annee
+            // 
+            this.tb_ca_annee.Location = new System.Drawing.Point(174, 138);
+            this.tb_ca_annee.Name = "tb_ca_annee";
+            this.tb_ca_annee.Size = new System.Drawing.Size(100, 20);
+            this.tb_ca_annee.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.checkBox1.Font = new System.Drawing.Font("Poor Richard", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.Red;
+            this.checkBox1.Location = new System.Drawing.Point(32, 178);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(242, 42);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Cocher cette case si les valeurs\r\n sont les mêmes pour chaques année";
+            this.checkBox1.UseVisualStyleBackColor = false;
             // 
             // Fsaisie
             // 
@@ -240,29 +316,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(804, 523);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.gb_infosAnnée);
+            this.Controls.Add(this.gb_infosProjet);
+            this.Controls.Add(this.cb_MemeValeur);
             this.Controls.Add(this.btn_valider);
             this.Controls.Add(this.btn_annuler);
-            this.Controls.Add(this.tb_ca);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.tb_cv);
-            this.Controls.Add(this.tb_cf);
-            this.Controls.Add(this.tb_txAct);
-            this.Controls.Add(this.tb_ann);
-            this.Controls.Add(this.tb_inv);
             this.Name = "Fsaisie";
             this.Text = "Fsaisie";
             this.Load += new System.EventHandler(this.Fsaisie_Load);
+            this.gb_infosProjet.ResumeLayout(false);
+            this.gb_infosProjet.PerformLayout();
+            this.gb_infosAnnée.ResumeLayout(false);
+            this.gb_infosAnnée.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -285,10 +351,15 @@
         private System.Windows.Forms.TextBox tb_ca;
         private System.Windows.Forms.Button btn_annuler;
         private System.Windows.Forms.Button btn_valider;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox cb_MemeValeur;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox gb_infosProjet;
+        private System.Windows.Forms.GroupBox gb_infosAnnée;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox tb_ca_annee;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
