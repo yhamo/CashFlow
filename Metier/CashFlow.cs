50ff96dbb1f0c46e99a3f660cad1f81a25cc6f55
nicolasfaure9 <nicolas.faure69@hotmail.fr>
2014-11-18 10:36:20 +0100

    pa

---
+++ Metier/CashFlow.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metier
+{
+    public class CashFlow
+    {
+        //Attributs
+        private double ca;
+        private double chargesFixes;
+        private double investissement;
+        private double nbAnnee;
+        private double txActualisation;
+        private double chargesVariables;
+
+        //Accesseurs
+        public double Ca
+        {
+            get { return ca; }
+            set { ca = value; }
+        }
+
+
+        public double Investissement
+        {
+            get { return investissement; }
+            set { investissement = value; }
+        }
+
+
+        public double NbAnnee
+        {
+            get { return nbAnnee; }
+            set { nbAnnee = value; }
+        }
+
+
+        public double TxActualisation
+        {
+            get { return txActualisation; }
+            set { txActualisation = value; }
+        }
+
+        public double ChargesVariables
+        {
+            get { return chargesVariables; }
+            set { chargesVariables = value; }
+        }
+
+
+        public double ChargesFixes
+        {
+            get { return chargesFixes; }
+            set { chargesFixes = value; }
+        }
+
+        public CashFlow()
+        { }
+
+
+    }
+}
--- Metier/Properties/AssemblyInfo.cs
+++ Metier/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("35d98472-6399-4115-8b58-1baf2bdd2d02")]
+[assembly: Guid("77f03f11-5e5d-4b5a-aaff-a418f3f800dc")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
